81579e06c107204de165d07095fa6c068cde5d8d
Ngugisenior <jmuiruri@nilefeeds.com>
2020-09-22 09:30:13 +0200

    first commit

---
+++ BulletedListSample/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="BulletedListSample.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>Bullet Styles</h1>
+            <asp:BulletedList ID="BlStyles" runat="server" BulletStyle="Numbered" DisplayMode="LinkButton" OnClick="BlStyles_Click">
+            </asp:BulletedList>
+            <asp:Label ID="LblMessage" runat="server"></asp:Label>
+        </div>
+        
+    </form>
+</body>
+</html>
---
+++ BulletedListSample/Default.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BulletedListSample
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                // Bullet Styles is an Enumerate type so we use the Enumerator class
+                foreach (string style in Enum.GetNames(typeof(BulletStyle)))
+                {
+                    BlStyles.Items.Add(style);
+                }
+                LblMessage.Text = "";
+            }
+        }
+
+        protected void BlStyles_Click(object sender, BulletedListEventArgs e)
+        {
+            string styleName = BlStyles.Items[e.Index].Text;
+            BulletStyle style = (BulletStyle)Enum.Parse(typeof(BulletStyle), styleName);
+            BlStyles.BulletStyle = style;
+            if (styleName=="Customimage")
+            {
+                BlStyles.BulletImageUrl = @"~\cellpic.png";
+            }
+
+            LblMessage.Text = "Your choice is: " + styleName;
+        }
+    }
+}
